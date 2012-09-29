b3ece6589f947a3f4c925d4237550b39d1cb6430
Michael Klatt <mdklatt@ou.edu>
2012-09-29 14:57:26 -0500

    packaging changes

--- package.php
+++ package.php
@@ -1,30 +1,24 @@
 <?php
-/* Package required files as a phar archive for distribution.
+/**
+ * Package library files as a phar archive for distribution.
+ *
+ * For security, the ability to create phar archives is disabled by default. To 
+ * change this the php.ini file must contain the following line:
+ *     phar.readonly = Off
+ */
 
-For security, the ability to create phar archives is disable by default. To 
-change this the php.ini file must contain the following line:
-     phar.readonly = Off
-
-*/
-
-// Check for the correction configuration in php.ini.
-
-if (!Phar::canWrite()) {
-	throw new Exception('php.ini prohibits the creation of phar archives');
-}
-
-
-// Define the package. In the future this would be a good place to use a 
+// Package definition. In the future this would be a good place to use a 
 // configuration file.
 
+require_once 'acis/acis.php';
 $pkg_name = 'acis';
 $pkg_init = "{$pkg_name}.php";
-$pkg_ver = '0.1.dev';
-$pkg_path = 'lib';
+$pkg_ver = ACIS_VERSION;
+$pkg_path = $pkg_name;
 $pkg_dest = 'dist';
 
 
-// Create the phar archive.
+// Create archive.
 
 if (!is_dir($pkg_dest) && !mkdir($pkg_dest, 0755, true)) {
 	throw Exception("could not create directory {$pkg_dest}");
@@ -32,6 +26,6 @@ if (!is_dir($pkg_dest) && !mkdir($pkg_dest, 0755, true)) {
 $phar_name = "{$pkg_name}-${pkg_ver}.phar";
 $phar_path = $pkg_dest.DIRECTORY_SEPARATOR.$phar_name;
 $phar = new Phar($phar_path, 0, $phar_name);
-$phar->buildFromDirectory($pkg_path);
-$phar->setStub(Phar::createDefaultStub('acis.php'));
+$phar->buildFromDirectory($pkg_path, '/\.php/');
+$phar->setStub(Phar::createDefaultStub($pkg_init));
 printf("%d file(s) added to archive {$phar_path}".PHP_EOL, count($phar));
--- test/_env.php
+++ test/_env.php
@@ -7,7 +7,7 @@ path is modified so that the local version of the library is imported.
 */
 
 define('TEST_PATH', dirname(__FILE__));
-define('LIB_PATH', dirname(TEST_PATH).DIRECTORY_SEPARATOR.'lib');
+define('LIB_PATH', dirname(TEST_PATH).DIRECTORY_SEPARATOR.'acis');
 
 chdir(TEST_PATH);
 set_include_path(LIB_PATH.PATH_SEPARATOR.get_include_path());

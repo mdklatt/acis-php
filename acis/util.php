28805dc580a36faf8a2284ad4bec12fc1f5ce082
Michael Klatt <mdklatt@ou.edu>
2013-05-24 10:10:24 -0500

    unknown network types no longer causes an exception

--- acis/util.php
+++ acis/util.php
@@ -4,12 +4,13 @@
 /**
  * Return an associative array of site IDs keyed by their ID type.
  *
- * The sids parameter is an array of SIDs from ACIS metadata.
+ * The sids parameter is an array of SIDs from ACIS metadata. There can be 
+ * more than on SID per network type.
  */
 function ACIS_decodeSids($sids)
 {
     $SID_REGEX = '/^([^ ]*) (\d+)$/';
-    $SID_TYPES = array(        
+    $NETWORK_TYPES = array(        
          1 => 'WBAN',      2 => 'COOP',      3 => 'FAA',       4 => 'WMO',       
          5 => 'ICAO',      6 => 'GHCN',      7 => 'NWSLI',     8 => 'RCC',  
          9 => 'ThreadEx',  9 => 'ThreadEx', 10 => 'CoCoRaHS', 16 => 'AWDN',
@@ -20,11 +21,9 @@ function ACIS_decodeSids($sids)
         if (!preg_match($SID_REGEX, $sid, $matches)) {
             throw new InvalidArgumentException("invalid sid: {$sid}");
         }
-        list(, $ident, $code) = $matches;
-        if (!($sid_type = ACIS_arrayGetKey($SID_TYPES, $code))) {
-            throw new InvalidArgumentException("invalid sid type: {$code}");
-        }
-        $sid_list = &ACIS_arraySetKey($table, $sid_type, array());
+        list(, $ident, $ntype) = $matches;
+        $network = ACIS_arrayGetKey($NETWORK_TYPES, $ntype, $ntype);
+        $sid_list = &ACIS_arraySetKey($table, $network, array());
         $sid_list[] = $ident;
     }
     return $table;
--- test/test_util.php
+++ test/test_util.php
@@ -10,18 +10,19 @@ require_once 'acis.php';
 
 class DecodeSidsFunctionTest extends PHPUnit_Framework_TestCase
 {
-    public function testNormal()
+    /**
+     * Test normal operation.
+     *
+     */
+    public function test()
     {
-        /**
-         * Test normal operation.
-         *
-         */
-        $sids = array('13967 1', '346661 2', '346664 2');
-        $table = array(
+        $encoded = array('13967 1', '346661 2', '346664 2', 'A123 9999');
+        $decoded = array(
             'WBAN' => array('13967'), 
-            'COOP' => array('346661', '346664')
+            'COOP' => array('346661', '346664'),
+            '9999' => array('A123')
         );
-        $this->assertEquals($table, ACIS_decodeSids($sids));
+        $this->assertEquals($decoded, ACIS_decodeSids($encoded));
         return;
     }
 }
